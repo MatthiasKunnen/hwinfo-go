0c85406632344922151f6075f105b5dba4737da9
Matthias Kunnen <matthias.kunnen@gmail.com>
2023-09-18 00:31:41 +0000

    Extract HWiNFO string types to separate file

--- pkg/hwinfoshmem/memory_reader_windows.go
+++ pkg/hwinfoshmem/memory_reader_windows.go
@@ -10,35 +10,8 @@ import (
 const (
 	hwinfoMutexName            = "Global\\HWiNFO_SM2_MUTEX"
 	hwinfoSensorsMapFilename   = "Global\\HWiNFO_SENS_SM2"
-	hwinfoSensorStringLength   = 128
-	hwinfoUnitStringLength     = 16
 	hwinfoSharedMemoryMaxBytes = 20_000_000
 )
-
-// HwinfoSensorStringAscii is a fixed length byte array of 8-bit ASCII encoded characters.
-// The specific extended ASCII codepage used depends on the system's locale.
-//
-// Get the codepage used by your system using this powershell command:
-//
-//	[System.Text.Encoding]::Default
-//
-// The string it contains is padded by nul bytes.
-type HwinfoSensorStringAscii = [hwinfoSensorStringLength]byte
-
-// HwinfoSensorStringUtf8 is a fixed length byte array of UTF-8 encoded characters.
-// The string it contains is padded by nul bytes.
-// To convert it to a string, use
-// [github.com/MatthiasKunnen/hwinfo-go/pkg/util.NulTerminatedUtf8ByteArrayToString].
-// It is used in labels for sensor and reading.
-type HwinfoSensorStringUtf8 = [hwinfoSensorStringLength]byte
-
-// HwinfoUnitStringAscii is the same as [HwinfoSensorStringAscii] but used for unit strings such as
-// °C and MHz.
-type HwinfoUnitStringAscii = [hwinfoUnitStringLength]byte
-
-// HwinfoUnitStringUtf8 is the same as  [HwinfoSensorStringUtf8] but used for unit strings such as
-// °C and MHz.
-type HwinfoUnitStringUtf8 = [hwinfoUnitStringLength]byte
 
 // MemoryReader allows for reading the shared memory provided by HWiNFO.
 // Create an instance using NewMemoryReader.
---
+++ pkg/hwinfoshmem/string_types.go
@@ -0,0 +1,31 @@
+package hwinfoshmem
+
+const (
+	hwinfoSensorStringLength = 128
+	hwinfoUnitStringLength   = 16
+)
+
+// HwinfoSensorStringAscii is a fixed length byte array of 8-bit ASCII encoded characters.
+// The specific extended ASCII codepage used depends on the system's locale.
+//
+// Get the codepage used by your system using this powershell command:
+//
+//	[System.Text.Encoding]::Default
+//
+// The string it contains is padded by nul bytes.
+type HwinfoSensorStringAscii = [hwinfoSensorStringLength]byte
+
+// HwinfoSensorStringUtf8 is a fixed length byte array of UTF-8 encoded characters.
+// The string it contains is padded by nul bytes.
+// To convert it to a string, use
+// [github.com/MatthiasKunnen/hwinfo-go/pkg/util.NulTerminatedUtf8ByteArrayToString].
+// It is used in labels for sensor and reading.
+type HwinfoSensorStringUtf8 = [hwinfoSensorStringLength]byte
+
+// HwinfoUnitStringAscii is the same as [HwinfoSensorStringAscii] but used for unit strings such as
+// °C and MHz.
+type HwinfoUnitStringAscii = [hwinfoUnitStringLength]byte
+
+// HwinfoUnitStringUtf8 is the same as  [HwinfoSensorStringUtf8] but used for unit strings such as
+// °C and MHz.
+type HwinfoUnitStringUtf8 = [hwinfoUnitStringLength]byte
